d3bfba5fc210c3336da040a76143428e48367207
Kartik Pawar <33437982+KartikPawar24@users.noreply.github.com>
2018-06-08 20:04:11 +0530

    Add files via upload

---
+++ ListView_Search_using_Xamarin_Forms/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ListView_Search_using_Xamarin_Forms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ListView_Search_using_Xamarin_Forms.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ListView_Search_using_Xamarin_Forms/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListView_Search_using_Xamarin_Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> names = new List<string>
+        {
+            "Kartik","Dinesh","Deepak","Rahul","Amar","Samson"
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NamesListView.ItemsSource = names;
+        }
+       void Search_btnPressed(object sender,EventArgs e)
+        {
+            var keyword = MainSearchBar.Text;
+
+            NamesListView.ItemsSource=
+                names.Where(name =>name.ToLower().Contains(keyword.ToLower()));
+        }
+    }
+}
